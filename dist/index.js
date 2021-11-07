c967459a89b762897c4a39ef6d733e0a06914354
Alex <alguienqueconoces26@gmail.com>
2021-11-07 17:05:56 -0500

    a

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Klar_1 = __importDefault(require("./Klar"));
-let client = new Klar_1.default();
-client.setup();
--- dist/scopes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-require("dotenv").config();
-module.exports = {
-    clientID: process.env.clientID,
-    clientSecret: process.env.clientSecret,
-    callbackURL: process.env.callbackURL,
-    scope: ["identify", "guilds"],
-    token: process.env.token,
-};
