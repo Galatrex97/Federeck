e9ef0e4967876027b4ac859d3926fbf6a7047740
Alex <alguienqueconoces26@gmail.com>
2021-11-08 11:49:54 -0500

    a

--- src/Distube/error.ts
+++ src/Distube/error.ts
@@ -1,9 +1,10 @@
 
+import { TextChannel } from "discord.js";
 import { DistubeEvent } from "../Interfaces";
 
 export const distubeEvent: DistubeEvent = {
     name: "error",
-run: async (channel, error) => {
+run: async (channel: TextChannel, error: Error) => {
     channel.send(`Ha ocurrido un error: **${error}**`)
 }
 }
