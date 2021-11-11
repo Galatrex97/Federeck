60e4d6fb95198b314e10da8f65c4dea10ae56f8f
Alex <alguienqueconoces26@gmail.com>
2021-11-11 07:51:45 -0500

    si

--- src/Player/songAdd.ts
+++ src/Player/songAdd.ts
@@ -4,7 +4,7 @@ import { PlayerEvent } from "../Interfaces";
 export const dmpEvent: PlayerEvent = {
     name: "songAdd",
 run: async (client, queue, song) => {
-    let { msg } = queue.nowPlaying.data;
+    let { msg } = await queue.nowPlaying.data;
     await msg.channel.send(`Se añadió **${song}** a la lista de reproducción.`);
 }
 }
