634ba25a19fac60a54030a0d687e0215f8e52b4a
Alex <alguienqueconoces26@gmail.com>
2021-11-22 11:51:39 -0500

    Buttons calculator

--- src/Commands/utils/math.ts
+++ src/Commands/utils/math.ts
@@ -1,7 +1,7 @@
 import math from "math-expression-evaluator"; // Este NPM es con el que se podrá hacer los calculos
 import Discord, { Client, Message, MessageEmbed } from "discord.js";
 import Klar from "../../Client";
-
+import simplydjs from "simply-djs";
 import { Command } from "../../Interfaces";
 
 export const command: Command = {
@@ -14,31 +14,9 @@ export const command: Command = {
     "Una calculadora para resolver operaciones matematicas de forma sencilla.",
 
   run: async (client, message, args) => {
-    const embed = new Discord.MessageEmbed().setColor(`WHITE`);
-
-    if (!args[0]) {
-      embed.setFooter("Por favor escribe una `expresión`.");
-      return await message.reply({ embeds: [embed] }); // Devuelve un mensaje si es que ejecuta el comando sin nada
-    }
-    let resultado: any;
-    try {
-      resultado = math.eval(args.join(" ")); // El Args toma el calculo
-    } catch (e) {
-      let errmsg = new (require("discord.js").MessageEmbed)()
-        .setTitle("Ha ocurrido un error")
-        .setDescription(`**Tengo el siguiente error:** ${e}`)
-        .setThumbnail(`https://media.giphy.com/media/mq5y2jHRCAqMo/giphy.gif`)
-        .setFooter("Tipico")
-        .setColor("WHITE")
-        .setTimestamp();
-
-      resultado = "Error: Entrada Invalida"; // Cuando es incorrecta
-    }
-    embed
-      .addField("Entrada:", `\`\`\`js\n${args.join(" ")}\`\`\``, false) // Te da el calculo
-      .setTitle("Calculadora de Lyon")
-      .addField("Resultado", `\`\`\`js\n${resultado}\`\`\``, false);
-    await message.channel.send({ embeds: [embed] });
+    simplydjs.calculator(message as any, {
+      embedColor: "#ffffff"
+    });
   },
 };
 
