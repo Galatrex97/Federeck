219c53bbc5adccf2d2aeeeb9fcfc6befd99ec1e8
Alex <alguienqueconoces26@gmail.com>
2021-11-26 14:41:10 -0500

    Potater

--- src/Commands/config/hailgrasa.ts
+++ src/Commands/config/hailgrasa.ts
@@ -30,15 +30,11 @@ const params = {
       return message.channel.send("Tienes que especificar. (on/off)");
 
     if (args[0] === "on") {
-      await la_grasa.findOne(params,
-        async (err: any, data: any) => {
+      let data = await la_grasa.findOne(params); 
           if (data?.sdlg == true) {
             return message.reply({
               content: `Las respuestas a los ":v" ya estaban activadas. Usa \`${p}pacman off\` para desactivarlas.`,
             });
-          }
-          if (err) {
-            console.log(err);
           }
 
           if (data) {
@@ -55,17 +51,13 @@ const params = {
             }).save();
             message.reply({ content: "#HailGrasa" });
           }
-        }
-      );
     } else if (args[0] === "off") {
-      await la_grasa.findOne(params,
-        async (err: any, data: any) => {
+     let data = await la_grasa.findOne(params);
           if (data?.sdlg == false) {
             return message.reply({
               content: `Las respuestas a los pacmans ya estaban desactivadas. Prueba \`${p}pacman on\` para activarlas.`,
             });
           }
-          if (err) console.log(err);
           if (!data) {
             return message.reply({
               content: `Las respuestas a los pacmans vienen deshabilitadas por defecto. Usa \`${p}pacman on\` para activarlas por primera vez.`,
@@ -81,8 +73,6 @@ const params = {
                 "Las respuestas a los pacmans han sido desactivadas. :'v",
             });
           }
-        }
-      );
     } else {
       return message.channel.send("Ese argumento no es válido. Usa on/off.");
     }
