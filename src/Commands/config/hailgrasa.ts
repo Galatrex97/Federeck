3a66727bd913ac2b36c9e3b3af8826e23b71ada5
Alex <alguienqueconoces26@gmail.com>
2021-11-26 14:33:09 -0500

    Evil Forelli

--- src/Commands/config/hailgrasa.ts
+++ src/Commands/config/hailgrasa.ts
@@ -12,6 +12,11 @@ export const command: Command = {
   description: "",
 
   run: async (client, message, args, p) => {
+
+const params = {
+  guildId: message.guild?.id
+}
+
     if (!message.member?.permissions.has("MANAGE_MESSAGES"))
       return message
         .reply("Necesitas el permiso **Gestionar mensajes**.")
@@ -25,8 +30,7 @@ export const command: Command = {
       return message.channel.send("Tienes que especificar. (on/off)");
 
     if (args[0] === "on") {
-      await la_grasa.findOne(
-        { guildId: message.guild?.id },
+      await la_grasa.findOne(params,
         async (err: any, data: any) => {
           if (data?.sdlg == true) {
             return message.reply({
@@ -38,14 +42,13 @@ export const command: Command = {
           }
 
           if (data) {
-            await la_grasa.findOneAndDelete({ guildId: message.guild?.id });
+            await la_grasa.findOneAndDelete(params);
             data = await new la_grasa({
               guildId: message.guild?.id,
               sdlg: true,
             }).save();
             message.reply({ content: "#HailGrasa" });
-          }
-          if (!data) {
+          } else if (!data) {
             data = await new la_grasa({
               guildId: message.guild?.id,
               sdlg: true,
@@ -55,8 +58,7 @@ export const command: Command = {
         }
       );
     } else if (args[0] === "off") {
-      await la_grasa.findOne(
-        { guildId: message.guild?.id },
+      await la_grasa.findOne(params,
         async (err: any, data: any) => {
           if (data?.sdlg == false) {
             return message.reply({
@@ -69,7 +71,7 @@ export const command: Command = {
               content: `Las respuestas a los pacmans vienen deshabilitadas por defecto. Usa \`${p}pacman on\` para activarlas por primera vez.`,
             });
           } else if (data) {
-            await la_grasa.findOneAndDelete({ guildId: message.guild?.id });
+            await la_grasa.findOneAndDelete(params);
             data = await new la_grasa({
               guildId: message.guild?.id,
               sdlg: false,
