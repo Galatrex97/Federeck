161bce5f3e7e7087f5cd6e4ab6bd1331abed11b7
Alex <alguienqueconoces26@gmail.com>
2021-11-27 09:08:51 -0500

    si

--- src/Events/interactionCreate.ts
+++ src/Events/interactionCreate.ts
@@ -21,7 +21,7 @@ let mentionData = await mentionSchema.findOne(parentParams) || await mentionSche
       cooldownMsg: "Ya tienes un ticket abierto, cierralo primero para abrir otro.",
       timeout: false,
       embedColor: "#ffffff",
-      pingRole: mentionData.mentions
+      pingRole: mentionData.mentions.daRoles
     });
 
     if (interaction.isCommand()) {
