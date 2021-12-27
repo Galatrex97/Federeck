ddff5a5c2b52fc46c2f6326647b9c4ffe9637ec8
Alex <alguienqueconoces26@gmail.com>
2021-12-27 11:03:44 -0500

    asd

--- src/Commands/info/userinfo.ts
+++ src/Commands/info/userinfo.ts
@@ -68,7 +68,7 @@ export const command: Command = {
     }
     }
 
-let estado = 
+let estado = `${user.presence.activities[0].state || "Sin estado"}`
 
     let final;
     if(!user.user.bot && user?.presence.activities[0] && user?.presence?.activities[0].id == "custom") {
@@ -169,7 +169,7 @@ let estado =
         {
           name: "Estado: ", // Nombre - Titulo - Caso 1
           value: user?.presence?.activities[0].state
-            ? `\`${user?.presence?.activities[0].state || "Sin estado"}\``
+            ? `\`${estado}\``
             : "\`Sin estado\`", // Si el "user" tiene actividad se envia, si no la tiene se envia "Sin Estado"
             inline: false,
         },
