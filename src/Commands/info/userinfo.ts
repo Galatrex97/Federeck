099d618af43469fe029d68b5e19c3cf47fe91438
Alex <alguienqueconoces26@gmail.com>
2021-12-27 10:41:53 -0500

    a

--- src/Commands/info/userinfo.ts
+++ src/Commands/info/userinfo.ts
@@ -80,7 +80,7 @@ export const command: Command = {
       }
     } else if(activityType == "Escuchando") {
       if(user.presence.activities[1].name == "Spotify") {
-      final = `Escuchando Spotify\nNombre: ${user.presence.activities[1].details}\nArtista: ${user.presence.activities[1].state}`;
+      final = `Escuchando Spotify\nNombre: ${user.presence.activities[1].details}\nArtista: ${user.presence.activities[1].state}\n` + `Link: [Click Aquí](https://open.spotify.com/track/${user.presence.activities[0].syncId})`;
       } else {
         final = `Èscuchando ${user.presence.activities[1].name}`;
       }
@@ -100,7 +100,7 @@ export const command: Command = {
       }
     } else if(activityType == "Escuchando") {
       if(user.presence.activities[0].name == "Spotify") {
-      final = `Escuchando Spotify\nNombre: ${user.presence.activities[0].details}\nArtista: ${user.presence.activities[0].state}\n` + `Link: [Click Aquí](https://open.spotify.com/track/${})`;
+      final = `Escuchando Spotify\nNombre: ${user.presence.activities[0].details}\nArtista: ${user.presence.activities[0].state}\n` + `Link: [Click Aquí](https://open.spotify.com/track/${user.presence.activities[0].syncId})`;
       } else {
         final = `Escuchando ${user.presence.activities[0].name}`;
       }
