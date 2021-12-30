07cd1f27a88ed7f2275fc87edc559809fb38efae
Alex <alguienqueconoces26@gmail.com>
2021-12-30 10:07:08 -0500

    are yo from the ghetto?

--- src/Commands/utils/intents.ts
+++ src/Commands/utils/intents.ts
@@ -132,7 +132,7 @@ collector.on("collect", async(interaction) => {
     }
 
     embed.setDescription(`Tú número de intents: ${main}`)
-    interaction.editReply({ embeds: [embed], components: [row] })
+    message.edit({ embeds: [embed], components: [row] })
 })
 message.reply({ embeds: [embed], components: [row] })
 
