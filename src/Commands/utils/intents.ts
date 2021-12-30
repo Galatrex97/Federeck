503a3faa241250b9531db3bf3d6b4d226df8acf0
Alex <alguienqueconoces26@gmail.com>
2021-12-30 13:22:18 -0500

    a

--- src/Commands/utils/intents.ts
+++ src/Commands/utils/intents.ts
@@ -240,12 +240,12 @@ collector.on("collect", async(interaction) => {
     if(interaction.values.length >= 2) {
     for(let i = 0; i < interaction.values.length - 1; i++) {
         let intents = interaction.values[i];
-        finalEvent = events[intents].join("\n") + finalEvent;
+        finalEvent = events[intents].join("\n");
     }
     } else {
         for(let i = 0; i < interaction.values.length; i++) {
             let intents = interaction.values[i];
-            finalEvent = events[intents].join("\n") + finalEvent;
+            finalEvent = events[intents].join("\n");
         }
     }
 
